6f1f93af237aa7c8f422c633a57f1874696dd7b9
Sebastian Orjuela <sebasorjuela@gmail.com>
2024-05-07 15:37:41 -0500

    First Commit

---
+++ src/main/java/talentsoft/sstback/SSTbackApplication.java
@@ -0,0 +1,13 @@
+package talentsoft.sstback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSTbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SSTbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/talentsoft/sstback/SSTbackApplicationTests.java
@@ -0,0 +1,13 @@
+package talentsoft.sstback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SSTbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
